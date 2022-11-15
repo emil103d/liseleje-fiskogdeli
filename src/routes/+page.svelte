0640a46c7db826b24f80ce3fd58399659b306343
emilie carlsen <emil103d@stud.kea.dk>
2022-11-15 13:43:48 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
