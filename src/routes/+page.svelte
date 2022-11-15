d9b2198055c030e5c39796f8d67b161da3a39dee
Soph0298 <Soph0298@stud.kea.dk>
2022-11-15 15:40:34 +0100

    Sophia test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,12 @@
 <h1 class="text-3xl font-bold underline bg-red-500">Hello world!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<p class="bg-pink-600"> Hej Reves</p>
+<p class="bg-pink-600">Hej Reves</p>
 
 <div class="flex justify-center bg-yellow-500 max-w-7xl">
-    <p>
-        TEst
+	<p>TEst</p>
+</div>
 
-    </p>
+<div class="flex justify-center bg-blue-500 max-w-7xl">
+	<p>Kanonen</p>
 </div>
