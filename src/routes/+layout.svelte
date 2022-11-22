7d522e8a507b8360cc915f1439730a844bc161a6
AmadeusNoah <amadeus.jensen@gmail.com>
2022-11-22 16:22:16 +0100

    nyhedsbrev uden billede

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css';
 	import Nav from "../components/nav.svelte";
+	import Nyhedsbrev from '../components/nyhedsbrev.svelte';
 	import Footer from "../components/footer.svelte";
 </script>
 
 <Nav />
 <slot />
+<Nyhedsbrev />
 <Footer />
