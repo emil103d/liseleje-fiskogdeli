c8fdd348a709446422a1ad2dc2eb10e1b5de473c
AmadeusNoah <amadeus.jensen@gmail.com>
2022-11-22 18:12:07 +0100

    Jeg slår potentielt nyhedsbrevsektionen ihjel v.1

--- src/components/nyhedsbrev.svelte
+++ src/components/nyhedsbrev.svelte
@@ -1,23 +1,107 @@
-<section class="h-64 grid justify-center items-center">
+<section class="">
+
+    <div class="py-20 lg:flex lg:max-w-[1200px] lg:w-[-moz-available] lg:m-auto">
+
+        <div class="">
+            <h1>TILMELD DIG VORES NYHEDSBREV</h1>
+            <p>SPAR 10% på din første ordre!</p>
+        </div>
     
-    <div class="">
-        <h4>TILMELD DIG VORES NYHEDSBREV</h4>
-        <p>Spar 10% på din første ordre</p>
-
-
-
-
-
-        <form action="" class="grid"> 
-            <label for="navn" >Navn</label>
-            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Indtast navn" aria-label="Full name">
-
-            <label for="">E-mail</label>
-            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Indtast e-mail" aria-label="E-mail">
-
-            <button class="bg-darkblue text-white">Tilmeld mig</button>
-        </form>
-
+        <div class="lg:flex lg:justify-between">
+            <form class="lg:flex lg:justify-between g">
+                    
+                <div class="lg:flex lg:justify-between gap-4">
+                    <div>
+                        <label for="navn" class="text-textcolor" >Navn</label>
+                        <input class="h-10 appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Indtast navn" aria-label="Full name">
+                    </div>
+            
+                    <div>
+                        <label for="e-mail" class="text-textcolor">E-mail</label>
+                        <input class="h-10 appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Indtast e-mail" aria-label="E-mail">
+                    </div>
+                </div>
+            
+                <div class="">
+                    <button class="bg-darkblue text-white h-10 w-32">Tilmeld mig</button>
+                </div>
+            
+            </form>
+        </div>
     </div>
+    
+
 </section>
-x
+
+
+
+
+
+
+
+
+
+    <!-- <div class="md:flex md:justify-between lg:grid lg:max-w-[1200px] lg:m-auto">
+    
+        <div class="pb-9 md:pb-0 md:grid md:items-center">
+            <div class="">
+                <h4 class="text-bold">TILMELD DIG VORES NYHEDSBREV</h4>
+                <p>Spar 10% på din første ordre</p>
+            </div>    
+        </div>
+
+        <div class="flex justify-between">
+            <div class="bg-attention h-10 w-10"> 
+                <div class="grid gap-4 lg:flex">
+                            <div>
+                                <label for="navn" class="text-textcolor" >Navn</label>
+                                <input class="h-10 appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Indtast navn" aria-label="Full name">
+                            </div>
+        
+                            <div>
+                                <label for="e-mail" class="text-textcolor">E-mail</label>
+                                <input class="h-10 appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Indtast e-mail" aria-label="E-mail">
+                            </div>
+                        </div>
+            </div>
+        
+        
+            <div class="bg-test2 h-10 w-10">
+                <div class="grid items-end">
+                    <button class="bg-darkblue text-white h-10 w-32">Tilmeld mig</button>
+                </div>
+            </div>
+        </div>
+
+            
+
+    </div> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+    section {
+        background-image: url("nyhedsbrev_baggrund.jpg");
+        background-size: cover;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
 	import '../app.css';
 	import Nav from "../components/nav.svelte";
+	import Nyhedsbrev from '../components/nyhedsbrev.svelte';
 	import Footer from "../components/footer.svelte";
+
 </script>
 
 <Nav />
 <slot />
+<Nyhedsbrev />
 <Footer />
