d4b28a27d944828395ee03572cb423cd24567549
emilie carlsen <emil103d@stud.kea.dk>
2022-11-22 12:25:55 +0100

    ...

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			darkblue: '#1f425f',
+			lightblue: '#f8fcfe',
+			white: '#ffffff',
+			black: '#0000',
+			textcolor: '#1f425f',
+			attention: '#E5C72E'
+		},
+
+		fontFamily: {
+			primarytext: ['Montserrat', 'sans-serif']
+		},
 		extend: {}
 	},
 	plugins: []
