8f51ec70302323d78499ef4cc592f67c3f2c7736
AmadeusNoah <amadeus.jensen@gmail.com>
2022-11-22 16:23:24 +0100

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script>
 	import '../app.css';
 	import Nav from "../components/nav.svelte";
-	import Nyhedsbrev from '../components/nyhedsbrev.svelte';
 	import Footer from "../components/footer.svelte";
 </script>
 
 <Nav />
 <slot />
-<Nyhedsbrev />
 <Footer />
