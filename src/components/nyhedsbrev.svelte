10e653e3fa5b2fd6db3e2721681036960b525424
AmadeusNoah <amadeus.jensen@gmail.com>
2022-11-22 16:20:18 +0100

    nyhedbrev oprettet - mangler billede

---
+++ src/components/nyhedsbrev.svelte
@@ -0,0 +1,23 @@
+<section class="h-64 grid justify-center items-center">
+    
+    <div class="">
+        <h4>TILMELD DIG VORES NYHEDSBREV</h4>
+        <p>Spar 10% på din første ordre</p>
+
+
+
+
+
+        <form action="" class="grid"> 
+            <label for="navn" >Navn</label>
+            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Indtast navn" aria-label="Full name">
+
+            <label for="">E-mail</label>
+            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Indtast e-mail" aria-label="E-mail">
+
+            <button class="bg-darkblue text-white">Tilmeld mig</button>
+        </form>
+
+    </div>
+</section>
+x
