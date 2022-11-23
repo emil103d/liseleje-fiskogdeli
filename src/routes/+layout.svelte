0356ab86e26d9b0fe78f02412941af3f998cc99c
emilie carlsen <emil103d@stud.kea.dk>
2022-11-23 09:06:21 +0100

    dd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css';
-	import Nav from "../components/nav.svelte";
-	import Footer from "../components/footer.svelte";
+	import Nav from '../components/nav.svelte';
+	import Footer from '../components/footer.svelte';
+	import Burgermenu from '../components/burgermenu.svelte';
 </script>
 
 <Nav />
+<Burgermenu />
 <slot />
 <Footer />
