8a1cfadc8083fffa63fde2ff5370efa251e284b9
AmadeusNoah <amadeus.jensen@gmail.com>
2022-11-23 14:27:25 +0100

    Instafeed oprettet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
 <script>
 	import '../app.css';
 	import Nav from '../components/nav.svelte';
-	import Nyhedsbrev from '../components/nyhedsbrev.svelte';
 	import Burgermenu from '../components/burgermenu.svelte';
+	import Nyhedsbrev from '../components/nyhedsbrev.svelte';
+	import Instafeed from '../components/Instafeed.svelte';
 	import Footer from '../components/footer.svelte';
+	
 </script>
 
 <Nav />
 <Burgermenu />
 <slot />
+<Instafeed />
 <Nyhedsbrev />
 <Footer />
