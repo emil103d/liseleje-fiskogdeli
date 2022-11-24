1ea769cbf082a82bb7a9c9e535c74ba0695c6b83
AmadeusNoah <amadeus.jensen@gmail.com>
2022-11-24 18:23:40 +0100

    cards 80% done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,19 @@
 	import '../app.css';
 	import Nav from '../components/nav.svelte';
 	import Burgermenu from '../components/burgermenu.svelte';
+	import Udvalg from '../components/Udvalg.svelte';
 	import Testimonials from '../components/Testimonials.svelte';
 	import Nyhedsbrev from '../components/nyhedsbrev.svelte';
 	import Howitworks from '../components/Howitworks.svelte';
 	import Instafeed from '../components/Instafeed.svelte';
 	import Footer from '../components/footer.svelte';
 	
+	
 </script>
 
 <Nav />
 <Burgermenu />
+<Udvalg />
 <Testimonials/>
 <Howitworks />
 <Instafeed />
