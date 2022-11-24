55ab9de8cdc02152754cd4265d554373506d38d2
emilie carlsen <emil103d@stud.kea.dk>
2022-11-24 14:20:30 +0100

    ...

--- src/components/label.svelte
+++ src/components/label.svelte
@@ -2,7 +2,7 @@
 	export let title;
 </script>
 
-<div class="bg-attention rounded-2xl p-4 max-h-8 flex align-center  ">
+<div class="bg-attention rounded-2xl p-4 max-h-8 flex items-center ">
 	<p class="text-white text-center text-sm ">
 		{title}
 	</p>
