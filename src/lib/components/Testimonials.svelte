615640f3366623f3c87cedb3df7469e774a71bcf
AmadeusNoah <amadeus.jensen@gmail.com>
2022-11-29 20:05:22 +0100

    teste mangler JS

--- src/lib/components/Testimonials.svelte
+++ src/lib/components/Testimonials.svelte
@@ -1,14 +1,22 @@
 <section>
     <div class="wrapper py-20 px-10">   
         <div class="flex md:justify-between lg:max-w-[1024px] lg:m-auto">
-            <div class="w-full h-full">
+            <div class="w-full h-full m-auto pr-10">
                 <h2 class="font-bold text-textcolor">Kunderne siger <b class="text-yellowdot"> . </b></h2>
 				<div class="h-[2px] w-20 bg-darkblue mb-6" />
-				<p class="pb-6">
+				<p class="pb-6 italic">
 					“Vi elsker Liseleje og har derfor startet Liseleje Fisk & Deli så du kan forkæles med frisk fisk løbet af hele sommeren. Vi er født og opvokset i området og har derfor en stor passion“
 				</p>
 
-                p
+                <p class="font-semibold">- Borat Jensen</p>
+
+                <div class="flex gap-[6px] place-content-center pt-6">
+                    <div class="w-2 h-2 rounded-full bg-yellowdot"></div>
+                    <div class="w-2 h-2 rounded-full bg-white border border-darkblue"></div>
+                    <div class="w-2 h-2 rounded-full border border-darkblue"></div>
+                    <div class="w-2 h-2 rounded-full border border-darkblue"></div>
+                    <div class="w-2 h-2 rounded-full border border-darkblue"></div>
+                </div>
                 
                 
                 
