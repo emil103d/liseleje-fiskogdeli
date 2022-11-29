46f38693168a6409092c4e2d5897a4b808654be3
AmadeusNoah <amadeus.jensen@gmail.com>
2022-11-29 20:32:02 +0100

    krog begyndt ej done

--- src/lib/components/fiskekrog.svelte
+++ src/lib/components/fiskekrog.svelte
@@ -1,5 +1,10 @@
-<section>   
-    <div>
-        
+<section class="none lg:block">   
+    <div class="fixed z-10 bottom-10 right-16">
+        <div>
+            <svg width="49" height="91" viewBox="0 0 49 91" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M42.5749 18.8511C46.4083 14.1845 51.7749 4.15112 42.5749 1.35112C33.3749 -1.44888 39.4083 13.1845 43.5749 20.8511L42.5749 69.3516C42.2417 77.5182 36.1751 93.0516 14.5751 89.8516C-7.0249 86.6516 1.90843 62.1849 9.0751 50.3516L11.5742 69.3516L6.57422 59.8516" stroke="#1F425F"/>
+                </svg>                
+        </div>
+
     </div>
 </section>
