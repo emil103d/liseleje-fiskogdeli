ad2502f68d5fc0e2a9769be0f8641e1f6913cba6
emilie carlsen <emil103d@stud.kea.dk>
2022-11-29 08:50:58 +0100

    Styr på filsturktur

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/sotres';
+</script>
+
+<!-- <nav class="flex justify-center">
+	<ul>
+		<p>Current URL: {$page.path}</p>
+		<li><a href="/" class=nav-active-route={$page.URL.pathname === '/'} />HOME</li>
+	</ul>
+</nav> -->
