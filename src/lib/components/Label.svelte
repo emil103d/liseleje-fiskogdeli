fa7bec37398e796fa58dea8d5ccb36c41db2d795
AmadeusNoah <amadeus.jensen@gmail.com>
2022-11-29 09:34:40 +0100

    ama fucker label op.

--- src/lib/components/Label.svelte
+++ src/lib/components/Label.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="bg-attention rounded-2xl p-4 max-h-8 flex items-center ">
-	<p class="text-white text-center text-sm ">
+	<p class="text-white text-center text-sm">
 		{title}
 	</p>
 </div>
