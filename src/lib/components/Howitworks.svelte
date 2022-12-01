786e89948410fb55a3c4f1b7a883ec1ba1e1fda7
AmadeusNoah <amadeus.jensen@gmail.com>
2022-12-01 11:40:24 +0100

    testimonial prikker fjernet

--- src/lib/components/Howitworks.svelte
+++ src/lib/components/Howitworks.svelte
@@ -13,17 +13,17 @@
 		<div class="grid justify-center text-center gap-6 md:flex md:justify-between">
 			<div class="">
 				<img src="howto_1.png" class="pb-4 h-48 w-auto md:h-64 md:w-full" alt="">
-				<p class="text-lightblue">1. Vælg dine yndlingsvarer <b class="text-yellowdot">.</b></p>
+				<p class="text-lightblue">1. Vælg dine yndlingsvarer</p>
 			</div>
 
 			<div class="">
 				<img src="howto_2.png" class="pb-4 h-48 w-auto md:h-64 md:w-full" alt="">
-				<p class="text-lightblue">2. Tilføj til kurv & betal <b class="text-yellowdot">.</b></p>
+				<p class="text-lightblue">2. Tilføj til kurv & betal </p>
 			</div>
 
 			<div class="">
 				<img src="howto_3.png" class="pb-4 h-48 w-auto md:h-64 md:w-full" alt="">
-				<p class="text-lightblue">3. Afhent i butik <b class="text-yellowdot">.</b></p>
+				<p class="text-lightblue">3. Afhent i butik </p>
 			</div>
 		</div>
 
