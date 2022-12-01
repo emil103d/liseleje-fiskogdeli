3388b2181826baa984778fccfadc9deee0ebadda
AmadeusNoah <amadeus.jensen@gmail.com>
2022-12-01 17:04:02 +0100

    padding og margener opdateret

--- src/lib/components/Udvalg.svelte
+++ src/lib/components/Udvalg.svelte
@@ -2,13 +2,13 @@
 	import Cards from "./Cards.svelte";
 </script>
 
-<section class="py-20 px-10">   
+<section class="py-32 md:py-44 px-10">   
     <div class="lg:max-w-[1024px] lg:m-auto">
-        <div class="text-textcolor pb-6">
+        <div class="text-textcolor">
             <h2>Fisk, Tapas & meget mere <b class="text-yellowdot">.</b></h2>
-            <div class="h-[2px] w-20 bg-darkblue"></div>
+            <div class="h-[2px] w-20 bg-darkblue mb-6 mt-2"></div>
         </div>
-        <div class="grid md:flex gap-4 md:justify-between">
+        <div class="grid md:flex gap-12 md:justify-between">
             <Cards image="udvalg_fiskskaldyr.webp" kategori="Fisk & Skaldyr" beskrivelse="Alt godt fra havet - friskfanget i kattegat." />
             <Cards image="udvalg_fisketapas.webp" kategori="Fisketapas" beskrivelse="Køb vores tapasfade eller bland dit eget."/>  
             <Cards image="udvalg_tilbehør.webp" kategori="Tilbehør" beskrivelse="Vin, tapanader, brød m.m."/>
