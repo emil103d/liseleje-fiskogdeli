f304f79afa0e6d661a1334af8b6928f1f648a37a
AmadeusNoah <amadeus.jensen@gmail.com>
2022-12-02 11:10:12 +0100

    layout tilrettet step 1

--- src/routes/blandselvtapas/+page.svelte
+++ src/routes/blandselvtapas/+page.svelte
@@ -1,24 +1,30 @@
 <script>
 	/** @type {import('./$types').PageData} */
+	import '/src/app.css';
 	import Button from '/src/lib/uielements/Button.svelte';
 	import Numericbutton from '/src/lib/uielements/Numericinputs.svelte';
 	import Maengdeguide from '/src/lib/components/Maengdeguide.svelte';
 </script>
 
-<main>
-	<div>
-		<h1>Vælg fiske tapas</h1>
-		<div class="bg-background p-2" />
-	</div>
+<main class="wrapper py-32 md:py-44 px-10">
 
-	<section class="lg:max-w-[1024px] lg:grid m-auto lg:align-middle gap-6 grid grid-cols-3 gap-12">
-		<Maengdeguide class="grid" />
-		<div class="col-span-2">
-			<h2>Hvor mange personer bliver I?</h2>
-			<Numericbutton />
-			<Button type="primary"><a href="/blandselvtapas/vaelgfisketapas">Vælg Tapasretter</a></Button>
-		</div>
-	</section>
+	
+	<div class="grid lg:max-w-[1024px] lg:m-auto">
+		<h2 class="font-bold ">Bland selv fisketapas <b class="text-yellowdot"> . </b></h2>
+
+
+		<section class="">
+
+			<Maengdeguide class="grid" />
+
+			<div class="col-span-2">
+				<h2>Hvor mange personer bliver I?</h2>
+				<Numericbutton />
+				<Button type="primary"><a href="/blandselvtapas/vaelgfisketapas">Vælg Tapasretter</a></Button>
+			</div>
+		</section>
+
+	</div>
 </main>
 
 <style>
@@ -26,11 +32,4 @@
 		background-color: #f8fcfe;
 	}
 
-	h2 {
-		font-family: 'Montserrat';
-		font-style: normal;
-		font-weight: 400;
-		font-size: 25px;
-		line-height: 24px;
-	}
 </style>
