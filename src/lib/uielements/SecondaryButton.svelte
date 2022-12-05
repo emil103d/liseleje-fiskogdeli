3e1a0a0b4e3f4348e6a8859d1a24b9edf349a9ad
emilie carlsen <emil103d@stud.kea.dk>
2022-12-05 16:46:38 +0100

    upgraded streg

--- src/lib/uielements/SecondaryButton.svelte
+++ src/lib/uielements/SecondaryButton.svelte
@@ -4,6 +4,13 @@
 
 <button class={type}>
 	<slot />
+	<svg viewBox="0 0 455.555 48" preserveAspectRatio="none" width="150" height="10">
+		<path
+			d="M0 2c37.962 0 37.962 44 75.924 44s37.962-44 75.924-44 37.962 44 75.923 44c37.962 0 37.962-44 75.925-44 37.965 0 37.965 44 75.929 44s37.965-44 75.93-44"
+		/><path
+			d="M0 2c37.962 0 37.962 44 75.924 44s37.962-44 75.924-44 37.962 44 75.923 44c37.962 0 37.962-44 75.925-44 37.965 0 37.965 44 75.929 44s37.965-44 75.93-44"
+		/></svg
+	>
 </button>
 
 <style>
@@ -14,28 +21,28 @@
 		font-family: 'Lato', sans-serif;
 		display: inline-block;
 		position: relative;
+		color: inherit;
+		text-decoration: none;
+		position: relative;
+		margin-bottom: 1rem;
 	}
 
-	button:before {
+	svg {
+		/* height: 10px; */
 		position: absolute;
-		content: '';
-		bottom: -15px;
-		left: 0;
-		height: 100%;
 		width: 100%;
-		background: url(/line.svg) repeat-x;
-		background-position: 0;
 	}
 
-	button:hover::before {
-		animation: waveline 4s linear infinite;
-		/* opacity: 0; */
-		transition: all ease 0.5s;
+	svg path {
+		fill: transparent;
+		stroke-width: 4px;
+		stroke: currentColor;
+		stroke-dasharray: 547.4111328125;
+		stroke-dashoffset: 547.4111328125;
+		transition: stroke-dashoffset 0.5s cubic-bezier(0.29, 0.68, 0.74, 1.02);
 	}
 
-	@keyframes waveline {
-		to {
-			background-position: -150px;
-		}
+	button:hover svg path {
+		stroke-dashoffset: 0;
 	}
 </style>
