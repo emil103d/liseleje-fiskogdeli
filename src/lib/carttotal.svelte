364adea65c257f7be7fdc742b88eb0f5d18ca6de
emilie carlsen <emil103d@stud.kea.dk>
2022-12-05 13:10:22 +0100

    ..

---
+++ src/lib/carttotal.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { cart } from '../stores/cart.js';
+
+	// const addToCart = (product) => {
+	// 	for (let item of $cart) {
+	// 		if (item.id === product.id) {
+	// 			product.quantity += 1;
+	// 			$cart = $cart;
+	// 			return;
+	// 		}
+	// 	}
+	// 	$cart = [...$cart, product];
+
+	// };
+</script>
+
+<section>
+	<p>Der er {$cart.length} retter</p>
+</section>
+
+<style>
+</style>
