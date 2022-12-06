bd42ceb9f53ce0fd3c6396de87ceb7d73a5721f9
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-12-06 19:13:36 +0100

    kan ikke huske

--- src/routes/blandselvtapas/(flow)/vaelgfisketapas/+page.svelte
+++ src/routes/blandselvtapas/(flow)/vaelgfisketapas/+page.svelte
@@ -108,7 +108,7 @@
 
 <main>
 
-	<div class="grid lg:max-w-[1024px] py-[150px] lg:m-auto place-content-center w-full md:h-[100vh]">
+	<div class="grid lg:max-w-[1024px] px-5 lg:px-0 py-[150px] lg:py-0 lg:m-auto place-content-center w-full md:h-[100vh]">
 		<div>
 			<h2 class="text-darkblue">Bland Selv Fisketapas <b class="text-yellowdot">.</b></h2>
 			<div class="h-[2px] w-20 bg-darkblue mb-6" />
