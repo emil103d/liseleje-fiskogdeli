546251252373864cc3f7caa60da10b02f186dffb
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-12-06 19:19:04 +0100

    layout påbegyndt tilbehør

--- src/routes/blandselvtapas/(flow)/afslut/+page.svelte
+++ src/routes/blandselvtapas/(flow)/afslut/+page.svelte
@@ -3,4 +3,19 @@
 	import Opsummering from '/src/lib/Opsummering.svelte';
 </script>
 
-<Opsummering />
+<main>
+	<div class="grid lg:max-w-[1024px] px-5 lg:px-0 py-[150px] lg:py-0 lg:m-auto place-content-center w-full md:h-[100vh]">
+		<Opsummering />
+	</div>
+	
+</main>
+
+
+
+
+<style>
+	main {
+		background-color: #f8fcfe;
+	}
+
+</style>
