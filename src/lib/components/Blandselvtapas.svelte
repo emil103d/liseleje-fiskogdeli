d328db3a9cd0b27c0f48561d972a172fc6f84f14
emilie carlsen <emil103d@stud.kea.dk>
2022-12-13 15:28:09 +0100

    ...

--- src/lib/components/Blandselvtapas.svelte
+++ src/lib/components/Blandselvtapas.svelte
@@ -11,12 +11,12 @@
 				<h2 class="font-bold ">Bland selv fisketapas <b class="text-yellowdot"> . </b></h2>
 				<div class="h-[2px] w-20 bg-darkblue mb-6 mt-2" />
 				<p class="pb-8">
-					Vik har endnu engang udvalgt de bedste og lækreste fisketapasretter. Vi bringer alt det
-					bedste fra havet til jer, og indhyller jer i smagsoplevelser fra start til slut. <br /> Kombiner
-					dine egne favoritter eller vælg en af vores menuer, vi glæder os til at se, hvad du finder
-					på.
+					Vi har endnu engang udvalgt de bedste og lækreste fisketapasretter. Vi bringer alt det
+					bedste fra havet til jer, og indhyller jer i smagsoplevelser fra start til slut. <br /> Gør
+					som alle andre livsnyder og tag din mad nede på stranden. Kombiner dine egne favoritter eller
+					vælg en af vores sammensætninger. Vi glæder os til at se, hvad du finder på.
 				</p>
-				<Button type="primary"><a href="/blandselvtapas">Bestil takeaway</a></Button>
+				<Button type="primary"><a href="/blandselvtapas">Bland tapas</a></Button>
 			</div>
 
 			<div class="w-full h-full skygge md:block">
