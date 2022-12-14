722e5b850b24a8a0ac482bca9614158155f0b868
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-12-14 12:54:59 +0100

    label done

--- src/lib/uielements/Label.svelte
+++ src/lib/uielements/Label.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="bg-attention rounded-2xl py-[2px] px-3 max-h-8 max-w-fit flex items-center ">
-	<p class="text-white text-center text-sm font-semibold">
+	<p class="text-sort text-center text-sm font-semibold">
 		{title}
 	</p>
 </div>
