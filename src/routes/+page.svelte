5a4f05db44c8b8d9532a681d725984c160e505a7
emilie carlsen <emil103d@stud.kea.dk>
2022-12-14 18:51:31 +0100

    Nu virker mini animationer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,19 +12,44 @@
 	import Instafeed from '../lib/components/Instafeed.svelte';
 	import Footer from '../lib/components/Footer.svelte';
 	import Burgermenu from '../lib/components/Burgermenu.svelte';
-	
+	import Saos from 'saos';
 </script>
-
 
 <Nav />
 <Burgermenu />
 <Herosection />
 <Fiskekrog />
+
 <Blandselvtapas />
+
 <Friskt_loyalt />
-<Udvalg />
+<Saos animation={'scale-in-center 0.5s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'}>
+	<Udvalg />
+</Saos>
 <Testimonials />
 <Howitworks />
 <Instafeed />
 <Nyhedsbrev />
 <Footer />
+
+<style>
+	@keyframes -global-fade-in {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	@keyframes -global-scale-in-center {
+		0% {
+			transform: scale(0.5);
+			opacity: 1;
+		}
+		100% {
+			transform: scale(1);
+			opacity: 1;
+		}
+	}
+</style>
