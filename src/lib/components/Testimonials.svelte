d7e92dec73a2e18a0e92bb292e07ba468e54c4bd
emilie carlsen <emil103d@stud.kea.dk>
2022-12-14 16:30:45 +0100

    ...

--- src/lib/components/Testimonials.svelte
+++ src/lib/components/Testimonials.svelte
@@ -1,6 +1,8 @@
 <section>
 	<div class="wrapper py-32 md:py-44 px-10 ">
-		<div class="flex md:justify-between lg:max-w-[1024px] lg:m-auto bg-white p-6 md:p-14 lg:p-20 skygge">
+		<div
+			class="flex md:justify-between lg:max-w-[1024px] lg:m-auto bg-white p-6 md:p-14 lg:p-20 skygge"
+		>
 			<div class="w-full h-full m-auto pr-10 ">
 				<h2 class="font-bold text-textcolor">Kunderne siger <b class="text-yellowdot "> . </b></h2>
 				<div class="h-[2px] w-20 bg-darkblue mb-6 mt-2 skygge" />
@@ -22,7 +24,11 @@
 			</div>
 
 			<div class="hidden w-full h-full md:block skygge2">
-				<img class="w-full h-full object-cover" src="testimonials.webp" alt="Frisk fisk pakkes i madpapir og fiskebutikkens logo klistres på" />
+				<img
+					class="w-full h-full object-cover"
+					src="/Instafeed/insta6.webp"
+					alt="Frisk fisk pakkes i madpapir og fiskebutikkens logo klistres på"
+				/>
 			</div>
 		</div>
 	</div>
@@ -36,15 +42,13 @@
 
 	@media only screen and (max-width: 768px) {
 		.skygge {
-		box-shadow: 20px 20px #e5f1f9;
+			box-shadow: 20px 20px #e5f1f9;
 		}
 	}
 
 	@media only screen and (min-width: 768px) {
 		.skygge2 {
-		box-shadow: 20px 20px #e5f1f9;
+			box-shadow: 20px 20px #e5f1f9;
 		}
 	}
-
-
 </style>
